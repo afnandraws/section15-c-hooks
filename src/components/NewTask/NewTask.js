48689e08022ef9214e5b57aae048af9039602435
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2022-11-19 16:39:41 +0000

    Add files via upload

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useHTTP from "../../hooks/use-http";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest } = useHTTP();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendRequest(
+      {
+        url: "https://udemy-react-http-a54d8-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
---
+++ src/hooks/use-http.js
@@ -0,0 +1,63 @@
+import { useCallback, useState } from "react";
+
+const useHTTP = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        headers: requestConfig.headers ? requestConfig.headers : {},
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHTTP;
+
+// const fetchTasks = async (taskText) => {
+//   setIsLoading(true);a
+//   setError(null);
+//   try {
+//     const response = await fetch(
+//       "https://udemy-react-http-a54d8-default-rtdb.firebaseio.com/tasks.json"
+//     );
+
+//     if (!response.ok) {
+//       throw new Error("Request failed!");
+//     }
+
+//     const data = await response.json();
+
+//     const loadedTasks = [];
+
+//     for (const taskKey in data) {
+//       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+//     }
+
+//     setTasks(loadedTasks);
+//   } catch (err) {
+//     setError(err.message || "Something went wrong!");
+//   }
+//   setIsLoading(false);
+// };
